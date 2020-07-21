6fe093708b9af57a888b4088015b3f0849d2fe7a
Tom Bendall <t.bendall15@imperial.ac.uk>
2020-07-21 09:45:45 +0100

    reorganise files

---
+++ experiments/final/stochastic/peakon_convergence_dx.py
@@ -0,0 +1,37 @@
+from peakondrake import *
+from netCDF4 import Dataset
+from datetime import datetime
+
+code = 'new_periodic_peakon_convergence_dx'
+Ld = 40.
+tmax = 0.1
+dt = 1e-5
+
+starttime = datetime.now()
+
+experiment(code, Ld, tmax,
+           resolutions=[500, 750, 1000, 1500, 2000],
+           dts=dt,
+           sigmas=0.2,
+           seeds=0,
+           schemes='hydrodynamic',
+           timesteppings='midpoint',
+           ics='periodic_peakon',
+           num_Xis=1,
+           Xi_family='double_sines',
+           alphasq=1.0,
+           c0=0.,
+           gamma=0.,
+           diagnostics=['u_error_with_sde', 'u_error_weak', 'p_pde', 'q_pde'],
+           fields_to_output=['u_sde', 'u_sde_weak'],
+           ndump=int(tmax / (1 * dt)),
+           field_ndump=int(tmax / (4 * dt)),
+           allow_fail=True,
+           peakon_equations=False,
+           true_peakon_data='new_true_periodic_peakon_data',
+           nXi_updates=1,
+           periodic=True)
+
+endtime = datetime.now()
+print(code)
+print('Total runtime was %i' % (endtime - starttime).seconds)
